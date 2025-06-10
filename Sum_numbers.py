5376881f171705e96885622f65a1d707df7e0b05
yadav-404 <yadav.da.789@gmail.com>
2025-06-10 17:43:45 +0545

    adding third number

---
+++ Sum_numbers.py
@@ -0,0 +1,15 @@
+# sum_two_numbers.py
+
+
+#summing three numbers
+
+# Take input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the second number: "))
+
+# Calculate the sum
+total = num1 + num2
+
+# Display the result
+print("The sum is:", total)
