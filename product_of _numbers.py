757deb5a538fbb6aabe89d728fba6f134595ca87
yadav-404 <yadav.da.789@gmail.com>
2025-06-10 18:40:41 +0545

    adding new file

---
+++ product_of _numbers.py
@@ -0,0 +1,14 @@
+# sum_two_numbers.py
+
+# Take input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+
+
+
+# Calculate the sum
+total = num1 + num2
+
+# Display the result
+print("The sum is:", total)
