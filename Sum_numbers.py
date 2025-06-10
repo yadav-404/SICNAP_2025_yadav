ba514c003a821322605988ccadef099b7de88164
yadav-404 <yadav.da.789@gmail.com>
2025-06-10 17:53:26 +0545

    add fourth number

--- Sum_numbers.py
+++ Sum_numbers.py
@@ -3,11 +3,13 @@
 
 #summing three numbers
 
+#summing fourth number
+
 # Take input from the user
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the second number: "))
-
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
 # Calculate the sum
 total = num1 + num2
 
