06b1bf6682bfeb7567cc008f9039554d27630754
yadav-404 <yadav.da.789@gmail.com>
2025-06-10 18:50:29 +0545

    product of three numbers

--- product_of _numbers.py
+++ product_of _numbers.py
@@ -1,14 +1,16 @@
-# sum_two_numbers.py
+
+
+# product three number
 
 # Take input from the user
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-
+num3 = float(input("Enter the third number: "))
 
 
 
 # Calculate the sum
-total = num1 + num2
+total = num1*num2*num3
 
 # Display the result
 print("The sum is:", total)
